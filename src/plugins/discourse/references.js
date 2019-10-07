c0ad117cc48b6405dd27a4d909ef543cf4d9aaaa
Dandelion Mané <decentralion@dandelion.io>
2019-10-07 11:04:15 -0600

    Parse Discourse references from hyperlinks
    The `discourse/references` module now has a `linksToReferences` method
    which extracts the parsed Discourse references from an array of
    hyperlinks. The method is tested.
    
    Test plan: Unit tests added; `yarn test` passes.
    
    This is progress towards [Discourse reference and mention detection][1].
    
    [1]: https://discourse.sourcecred.io/t/discourse-reference-mention-detection/270

--- src/plugins/discourse/references.js
+++ src/plugins/discourse/references.js
@@ -2,8 +2,33 @@
 
 const htmlparser2 = require("htmlparser2");
 
-export type Hyperlink = string;
+import {type TopicId} from "./fetch";
 
+export type DiscoursePostReference = {|
+  +type: "POST",
+  +topicId: TopicId,
+  +postIndex: number,
+  +serverUrl: string,
+|};
+
+export type DiscourseTopicReference = {|
+  +type: "TOPIC",
+  +topicId: TopicId,
+  +serverUrl: string,
+|};
+
+export type DiscourseUserReference = {|
+  +type: "USER",
+  +username: string,
+  +serverUrl: string,
+|};
+
+export type DiscourseReference =
+  | DiscoursePostReference
+  | DiscourseTopicReference
+  | DiscourseUserReference;
+
+export type Hyperlink = string;
 export function parseLinks(cookedHtml: string): Hyperlink[] {
   const links = [];
   const httpRegex = /^https?:\/\//;
@@ -22,4 +47,38 @@ export function parseLinks(cookedHtml: string): Hyperlink[] {
   parser.write(cookedHtml);
   parser.end();
   return links;
+}
+
+export function linksToReferences(
+  links: $ReadOnlyArray<Hyperlink>
+): DiscourseReference[] {
+  const server = "(https://[\\w.-]+)";
+  const topic = `${server}/t/[\\w-]+/(\\d+)`;
+  const post = `${topic}/(\\d+)`;
+  const params = "(?:\\?[\\w-=]+)?";
+
+  const topicRegex = new RegExp(`^${topic}${params}/?$`);
+  const postRegex = new RegExp(`^${post}${params}/?$`);
+  const userRegex = new RegExp(`^${server}/u/([\\w-]+)${params}/?$`);
+  const references: DiscourseReference[] = [];
+  let match = null;
+  for (const link of links) {
+    if ((match = link.match(postRegex))) {
+      references.push({
+        type: "POST",
+        topicId: +match[2],
+        serverUrl: match[1],
+        postIndex: +match[3],
+      });
+    } else if ((match = link.match(topicRegex))) {
+      references.push({type: "TOPIC", topicId: +match[2], serverUrl: match[1]});
+    } else if ((match = link.match(userRegex))) {
+      references.push({
+        type: "USER",
+        username: match[2],
+        serverUrl: match[1],
+      });
+    }
+  }
+  return references;
 }
--- src/plugins/discourse/references.test.js
+++ src/plugins/discourse/references.test.js
@@ -1,6 +1,6 @@
 // @flow
 
-import {parseLinks} from "./references";
+import {parseLinks, linksToReferences} from "./references";
 
 describe("plugins/discourse/references", () => {
   describe("parseLinks", () => {
@@ -25,6 +25,66 @@ describe("plugins/discourse/references", () => {
     });
     it("doesn't pick up anchor hrefs", () => {
       expect(parseLinks(`<a href="#foo">A Link</a>`)).toEqual([]);
+    });
+  });
+
+  describe("linksToReferences", () => {
+    it("works for topics", () => {
+      const hyperlinks = [
+        "https://sourcecred-test.discourse.group/t/123-a-post-with-numbers-in-slug/20",
+        "https://sourcecred-test.discourse.group/t/123-a-post-with-numbers-in-slug/20/",
+        "https://sourcecred-test.discourse.group/t/123-a-post-with-numbers-in-slug/20?u=d11",
+      ];
+      const reference = {
+        type: "TOPIC",
+        topicId: 20,
+        serverUrl: "https://sourcecred-test.discourse.group",
+      };
+      expect(linksToReferences(hyperlinks)).toEqual([
+        reference,
+        reference,
+        reference,
+      ]);
+    });
+    it("works for posts", () => {
+      const hyperlinks = [
+        "https://sourcecred-test.discourse.group/t/my-first-test-post/11/2?u=d11",
+        "https://sourcecred-test.discourse.group/t/my-first-test-post/11/2/",
+        "https://sourcecred-test.discourse.group/t/my-first-test-post/11/2",
+      ];
+      const reference = {
+        type: "POST",
+        topicId: 11,
+        postIndex: 2,
+        serverUrl: "https://sourcecred-test.discourse.group",
+      };
+      expect(linksToReferences(hyperlinks)).toEqual([
+        reference,
+        reference,
+        reference,
+      ]);
+    });
+    it("works for mentions", () => {
+      const hyperlinks = ["https://sourcecred-test.discourse.group/u/d11"];
+      const reference = {
+        type: "USER",
+        username: "d11",
+        serverUrl: "https://sourcecred-test.discourse.group",
+      };
+      expect(linksToReferences(hyperlinks)).toEqual([reference]);
+    });
+    it("doesn't find bad or malformed references", () => {
+      const hyperlinks = [
+        // Not a reference to anything in particular.
+        "https://sourcecred-test.discourse.group",
+        // No https == no go. We can be more permissive if needed.
+        "sourcecred-test.discourse.group/t/foo/120",
+        // There's a space at the front.
+        " https://sourcecred-test.discourse.group/t/foo/120",
+        // unexpected trailing stuff
+        "https://sourcecred-test.discourse.group/t/foo/120$$",
+      ];
+      expect(linksToReferences(hyperlinks)).toEqual([]);
     });
   });
 });
